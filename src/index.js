a3181bb288b48ad3a021ff09858f4e7d8e7ce7da
duduxiong <xianglinzong_cd@keruyun.com>
2018-12-27 17:16:42 +0800

    initial annotations

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@
  */
 
 function createThunkMiddleware(extraArgument) {
-  // 注意下面的dispatch其实被中间件增强
+  // 注意下面的dispatch其实被中间件增强，而参数next其实就是dispatch
+  // 在应用开发中，action函数执行后返回一个thunk函数，该thunk函数接受dispatch作为参数，即相当于next函数
   return ({ dispatch, getState }) => next => action => {
     if (typeof action === "function") {
       return action(dispatch, getState, extraArgument);
     }
 
-    // next其实也是一个变种的dispatch函数，经过composed的函数在执行时内部函数执行返回next
     return next(action);
   };
 }
