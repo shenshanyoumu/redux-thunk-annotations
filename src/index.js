a45d8240c4b1029ff3d5ba77f13f454fc9744c28
duduxiong <xiang_linzong@163.com>
2020-05-26 16:32:37 +0800

    docs: thunk编程形态来是实现异步action

--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@
  */
 
 function createThunkMiddleware(extraArgument) {
+
+  // 结合redux源码，中间件在applyMiddlewares函数中首先接受{dispatch,getState}参数
+  // 注意此时dispatch属性方法:(...args)=>dispatch(...args)，里面的dispatch通过闭包形式
+  // 得到一个增强型态；next是通过compose(...middlewares)(store.dispatch)构造的嵌套调用结构
+  
+
+  // 当action是普通的对象时，则next就是store.dispatch函数；而当action具有副作用
+  // 则通过
   return ({ dispatch, getState }) => next => action => {
     if (typeof action === "function") {
       return action(dispatch, getState, extraArgument);
     }
 
-    // redux中间件的“洋葱结构”，传递给下一个中间件。当action在所有中间件流动后，会通过原始的store.dispatch触发
+    // 洋葱模型，最里层中间件接受参数store.dispatch，然后返回的新函数接受普通的action对象
+    // 作为thunk调用的精髓，当内层函数执行完毕具有返回值后，再依次调用外层函数
     return next(action);
   };
 }
